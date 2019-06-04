e1d702f50426c0affda8edb99f59cc6710bda4b0
Eli <13095451128@163.com>
2019-06-04 17:54:29 +0800

    third commit
    Signed-off-by: Eli <13095451128@163.com>

--- test.py
+++ test.py
@@ -3,10 +3,27 @@
 
 import io
 import os
+import sys
 import Tkinter
 
 def clicked():
 	print 'clicked'
+
+def calculat():
+	x = 50
+	while x < 80:
+		x += 1
+		y = 170
+		while y > 130:
+			y -= 1
+			value = ((x ** 2) + (y ** 2)) ** 0.5
+			diff = abs(value - 161.29)
+			if diff < 0.1:
+				print "%d * %d * %lf" % (x, y, value)
+
+def createfile(name, size):
+    with open(name, 'wb') as f:
+            f.write(os.urandom(size))
 
 members = ['Eli Chang', 'Fei Wei', 'Morse Ma', 'Tsao yufeng', 'Gandalf Jiang', 'Hytt Hu']
 window = Tkinter.Tk()
