2d4f0d29060f2ffa76d9a2095b868427960e4e60
EliChang <eliflichang@gmail.com>
2020-06-06 18:03:14 +0800

    pachong

--- auto_cp.py
+++ auto_cp.py
@@ -1,30 +1,56 @@
-import os
-import commands
+import urllib.request
+import re
+# import PyV8
+# import execjs
+from bs4 import BeautifulSoup
 
-def cherry_pick(path, cmd):
-	os.chdir(path)
-	print(commands.getstatusoutput(change_name(cmd)))
+host = 'http://10.20.40.21:8081'
+targets = ['/c/Freeme/platforms/android-25/ALPS-MP-N1.MP15-V1_DROI6758_66_N1_6758/project/droi_g1930epq/+/34969']
 
-def change_name(cmd):
-	name_start = cmd.find('ssh://') + 6
-	name_end = cmd.find('@')
-	if 0 < name_start < name_end:
-		origin_name = cmd[name_start : name_end]
-		if origin_name != git_user:
-			return cmd.replace(origin_name, git_user)
-	return cmd
+first_header = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+	'Cache-Control': 'max-age=0',
+	'Connection': 'keep-alive',
+	'Cookie': 'GERRIT_UI=POLYGERRIT; GerritAccount=aScfprqTXx44dBi0lQGNRL-I5aDZuqVaYq; XSRF_TOKEN=aScfprsO1aGOedB64gHvQIilydlDgHUgIa',
+	'Host': '10.20.40.21:8081',
+	'Referer': 'http://10.20.40.21:8081/dashboard/self',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+}
 
-def read_cmds_from_file(path):
-	file = open(path, 'r')
-	str = file.read()
-	lines = str.splitlines()
-	for line in lines:
-		ele = line.split('#')
-		if len(ele) == 2:
-			path = ele[0]
-			cmd = ele[1]
-			cherry_pick(os.getcwd() + '/' + path, cmd)
+def add_header(request, headers):
+	for key in list(headers.keys()):
+		request.add_header(key, first_header.get(key))
 
-git_user = commands.getstatusoutput('git config user.name')[1];
+def save_file(raw, file):
+	with open (file, 'w') as file_object:
+		file_object.write(raw)
 
-read_cmds_from_file('cp.txt')
+def request(url, header, codec):
+	request = urllib.request.Request(url)
+	add_header(request, header)
+	response = urllib.request.urlopen(request)
+	
+	html_raw = ''
+	if codec == '':
+		html_raw = response.read()
+	else:
+		html_raw = response.read().decode(codec)
+	# print('response code', response.getcode())
+	return html_raw
+
+def html_2_soup(raw):
+	return BeautifulSoup(raw, 'html.parser')
+
+first_soup = html_2_soup(request(host + targets[0], first_header, 'utf8'))
+
+for link in first_soup.find_all('link'):
+	target = link.get('href')
+	if 'gr-app' in target:
+		link_url = host + target
+		print(link_url)
+		second_html = request(link_url, first_header, 'ISO-8859-1')
+		# print(second_html)
+		save_file(second_html, target.replace('/', '-') + '.html')
