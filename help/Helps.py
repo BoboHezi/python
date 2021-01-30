7fcc962e88a909277a70adc8847809bd38dd342a
EliChang <eliflichang@gmail.com>
2021-01-30 18:15:14 +0800

    help test

---
+++ help/Helps.py
@@ -0,0 +1,27 @@
+
+def main():
+	"""
+	This is a short command for adb, created by Eli.
+	you can input below options.
+
+	Options:
+		dev	enable Developer Mode and open it
+
+		start	going to activity
+	"""
+	pass
+
+def dev():
+	"""
+	enable Developer Mode and open it
+	"""
+	pass
+
+def start():
+	"""
+	going to activity
+	
+	-a [action]
+	-n [component]
+	"""
+	pass
---
+++ help/sc.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import re
+import time
+
+if ( __name__ == "__main__"):
+
+	help('Helps.main')
+
+	print('-' * 60)
+
+	help('Helps.dev')
+
+	print('-' * 60)
+
+	help('Helps.start')
